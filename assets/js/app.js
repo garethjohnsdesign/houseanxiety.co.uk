96d421ba99954cc145e0edda6262e6eb2281ef15
Gareth Johns <info@garethjohnsdesign.com>
2020-04-21 18:14:01 +0100

    Updated Yarn and 14 other files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,103 @@
+// 1. Imports
+// ----------
+
 import $ from "jquery";
 import 'what-input';
+import AOS from "aos";
+import Swup from 'swup';
+import SwupBodyClassPlugin from "@swup/body-class-plugin";
+import SwupScrollPlugin from '@swup/scroll-plugin';
+import SwupGaPlugin from '@swup/ga-plugin';
+import SwupPreloadPlugin from '@swup/preload-plugin';
+
+// Foundation JS relies on a global variable. In ES6, all imports are hoisted
+// to the top of the file so if we used `import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+/*
+window.jQuery = $;
+require('foundation-sites');
+*/
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
 import './lib/foundation-explicit-pieces';
 
+
+$( document ).ready( function() {
+  function init() {
+
+// 2. Foundation
+// ----------
+
+Foundation.Interchange.SPECIAL_QUERIES['medium-retina'] = 'only screen and (min-width: 40em), (min-width: 40em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 40em) and (min--moz-device-pixel-ratio: 2), (min-width: 40em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 40em) and (min-device-pixel-ratio: 2), (min-width: 40em) and (min-resolution: 192dpi), (min-width: 40em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['large-retina'] = 'only screen and (min-width: 64em), (min-width: 64em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 64em) and (min--moz-device-pixel-ratio: 2), (min-width: 64em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 64em) and (min-device-pixel-ratio: 2), (min-width: 64em) and (min-resolution: 192dpi), (min-width: 64em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['xlarge-retina'] = 'only screen and (min-width: 75em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
+Foundation.Interchange.SPECIAL_QUERIES['xxlarge-retina'] = 'only screen and (min-width: 90em), (min-width: 75em) and (-webkit-min-device-pixel-ratio: 2), (min-width: 75em) and (min--moz-device-pixel-ratio: 2), (min-width: 75em) and (-o-min-device-pixel-ratio: 2/1), (min-width: 75em) and (min-device-pixel-ratio: 2), (min-width: 75em) and (min-resolution: 192dpi), (min-width: 75em) and (min-resolution: 2dppx)';
+  
 $(document).foundation();
+
+
+// 3. Viewport Height Fix
+// ----------------------
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+const vh = window.innerHeight * 0.01;
+
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
+// 4. Animate on Scroll
+// --------------------
+
+$(function() {
+  AOS.init({ 
+   offset: 32,
+   easing: 'ease-in-out-quart', 
+   duration: 600
+   });   
+});
+
+$(function() {
+window.addEventListener('load', AOS.refresh);
+});
+
+
+}
+
+// 5. Page Transitions
+// -------------------
+const options = {
+  animationSelector: '[class*="swup-transition-"]',
+  containers: [ '#swup-body' ],
+  plugins: [
+    new SwupPreloadPlugin(),
+    new SwupBodyClassPlugin(),
+    new SwupGaPlugin(),
+    new SwupScrollPlugin({
+        doScrollingRightAway: false,
+        animateScroll: true,
+        scrollFriction: 0.3,
+        scrollAcceleration: 0.04
+    })
+  ]
+};
+
+const swup = new Swup( options );
+
+// 6. Run Once
+// -----------
+init();
+
+swup.on( 'contentReplaced', init );
+
+} );
